f3171d791ec14b4191c8a2961cd04aec7b105d27
Bob Ross <ministash14@gmail.com>
2018-07-22 22:31:22 -0700

    Changed the message that responds to you selecting 'no'

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,14 +7,8 @@ let Item = require("./itemConstructor");
 // My Database Connection///////////////////
 var connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
     port: 3306,
-  
-    // Your username
     user: "root",
-  
-    // Your password
     password: "1234",
     database: "bamazon"
 });
@@ -23,44 +17,46 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     runTheShop();
-  });
+});
 
-  function createItem() {
-      console.log("Inserting a new product...\n");
-      var query = connection.query(
-          "INSERT INTO products SET ?",
-          {
-              product_name: "Cheese",
-              department_name: "food",
-              price: 3,
-              stock_quantity: 10
 
-            },
-            function(err, res) {
-                console.log(res.affectedRows + " product inserted!\n");
-            }
-        );
-    }
-        
-        
-        const runTheShop = () => {
-            inquirer.prompt([
-                
-                {
-                    type: "confirm",
-                    name: "welcome",
-                    message: 'Welcome to the bootleg version of Amazon, "Bamazon". Are you ready to begin?',
-                },
-                
-                
-            ]).then(function(user){
-                if(user.welcome === true){
-                    console.log("You son of a bith");
-                    runTheShop();
-                    createItem();
+// Start of my program's functionality/////
+function createItem() {
+    console.log("Inserting a new product...\n");
+    var query = connection.query(
+        "INSERT INTO products SET ?",
+        {
+            product_name: "Cheese",
+            department_name: "food",
+            price: 3,
+            stock_quantity: 10
+
+        },
+        function(err, res) {
+            console.log(res.affectedRows + " product inserted!\n");
         }
-        
+    );
+}
+
+
+const runTheShop = () => {
+    inquirer.prompt([
+
+        {
+            type: "confirm",
+            name: "welcome",
+            message: 'Welcome to the bootleg version of Amazon, "Bamazon". Are you ready to begin?',
+        },
+
+
+    ]).then(function(user) {
+        if (user.welcome === !true) {
+            console.log("  Well if you're not going to use this bootleg service, try our other free products, such as 'Abay',"  + "\n" + 
+            " 'Pacebook', and 'Bamagram'. Have a great bootlegging day ya beautiful pirate!");
+                        process.exit();
+        }
+
     });
-    
+
 }
 
