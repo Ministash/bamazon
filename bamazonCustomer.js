b5ffd9b04dc1ff3f4e6c1347964a129e2820682f
Bob Ross <ministash14@gmail.com>
2018-07-22 21:59:10 -0700

    Created a function that adds an item if the user selects yes on the prompt. Working on making the full game now that I have the functionality down

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,31 +1,66 @@
+//My variables/////////////////////////////
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 let Item = require("./itemConstructor");
 
-let items = 15;
 
+// My Database Connection///////////////////
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "1234",
+    database: "bamazon"
+});
 
-const runTheShop = () => {
-    inquirer.prompt([
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    runTheShop();
+  });
+
+  function createItem() {
+      console.log("Inserting a new product...\n");
+      var query = connection.query(
+          "INSERT INTO products SET ?",
+          {
+              product_name: "Cheese",
+              department_name: "food",
+              price: 3,
+              stock_quantity: 10
+
+            },
+            function(err, res) {
+                console.log(res.affectedRows + " product inserted!\n");
+            }
+        );
+    }
         
-        {
-            type: "confirm",
-            name: "welcome",
-            message: 'Welcome to the bootleg version of Amazon, "Bamazon". Are you ready to begin?',
-        },
         
-        
-    ]).then(function(user){
-        if(user.welcome === true){
-            
-            console.log("You son of a bith");
-            items--;
-            console.log(items);
-            runTheShop();
+        const runTheShop = () => {
+            inquirer.prompt([
+                
+                {
+                    type: "confirm",
+                    name: "welcome",
+                    message: 'Welcome to the bootleg version of Amazon, "Bamazon". Are you ready to begin?',
+                },
+                
+                
+            ]).then(function(user){
+                if(user.welcome === true){
+                    console.log("You son of a bith");
+                    runTheShop();
+                    createItem();
         }
         
     });
     
 }
 
-runTheShop();
