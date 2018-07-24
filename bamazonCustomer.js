9a4d9a223cd5f6856f80ab37ec4e46de37ad33e6
Bob Ross <ministash14@gmail.com>
2018-07-23 20:07:35 -0700

    Changing the way the user goes through the screens

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 let Item = require("./itemConstructor");
+const userInput = process.argv;
+const userInputCommand = userInput[2];
 
 
 // My Database Connection///////////////////
@@ -21,24 +23,6 @@ connection.connect(function(err) {
 
 
 // Start of my program's functionality/////
-function createItem() {
-    console.log("Inserting a new product...\n");
-    var query = connection.query(
-        "INSERT INTO products SET ?",
-        {
-            product_name: "Cheese",
-            department_name: "food",
-            price: 3,
-            stock_quantity: 10
-
-        },
-        function(err, res) {
-            console.log(res.affectedRows + " product inserted!\n");
-        }
-    );
-}
-
-
 const runTheShop = () => {
     inquirer.prompt([
 
@@ -47,16 +31,107 @@ const runTheShop = () => {
             name: "welcome",
             message: 'Welcome to the bootleg version of Amazon, "Bamazon". Are you ready to begin?',
         },
+        {
+            type: "input",
+            name: "userName",
+            message: "What's your name pirate?"
+        },
+        {
+            type: "list",
+            name: "itemName",
+            message: "Welcome in! What can I get for you today?",
+            choices: ["Coors Light", "Kraft Cheese", "48oz of Salmon", "TV", "Hydro Flask",
+                "PS4", "Kidney", "Backpack", "G.I. Joe", "6oz of Liquid Courage"]
+        },
+        {
+            type: "input",
+            name: "itemAmount",
+            message: "How many of this item would you like to purchase?"
+
+        }
 
 
     ]).then(function(user) {
         if (user.welcome === !true) {
-            console.log("  Well if you're not going to use this bootleg service, try our other free products, such as 'Abay',"  + "\n" + 
-            " 'Pacebook', and 'Bamagram'. Have a great bootlegging day ya beautiful pirate!");
-                        process.exit();
+            console.log("  Well if you're not going to use this bootleg service, try our other free products, such as 'Piratebay'," + "\n" +
+                " 'Fakebook', and 'Pirategram'. Have a great bootlegging day ya beautiful pirate!");
+            process.exit();
+        } else {
+            if(isNaN(user.itemAmount)){
+                console.log("Hey man! We only deal in whole numbers here. Try again-");
+            }else{
+                itemManager(user.itemName, user.itemAmount);
+
+            }
         }
 
     });
 
 }
 
+const itemManager = (item) => {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// switch (item) {
+//     case "Coors Light": itemChooser(item);
+//         break;
+//     case "Kraft Cheese": itemChooser(item);
+//         break;
+//     case "48oz of Salmon": itemChooser(user.items);
+//         break;
+//     case "TV": itemChooser(user.items);
+//         break;
+//     case "Hydro Flask": itemChooser(user.items);
+//         break;
+//     case "PS4": itemChooser(user.items);
+//         break;
+//     case "Kidney": itemChooser(user.items);
+//         break;
+//     case "Backpack": itemChooser(user.items);
+//         break;
+//     case "G.I. Joe": itemChooser(user.items);
+//         break;
+//     case "6oz of Liquid Courage": itemChooser(user.items);
+//         break;
