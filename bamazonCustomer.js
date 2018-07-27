7fcf7e147a9247ab5f84af062b7d9ec71716e39b
Bob Ross <ministash14@gmail.com>
2018-07-27 01:40:12 -0700

    Finished working product for the first part of the homework. So aka V1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,24 @@
+/*
+
+Initially, I decided to divide each prompt into it's own function. 
+It felt like the easiest way return back to any prompt I wanted to, regardless of where I 
+had been in the squence of guiding the user. But, in the end it just made data management difficult because I had
+to pass a bunch of values between each function. On a smaller application 
+like this, it's not too awful. However, I could see how much more difficult
+it would be to manage all of those values throughout a larger program. Any advice
+on how to better keep track of values would be appreciated.
+
+*/
+
+
 //My variables/////////////////////////////
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-let Item = require("./itemConstructor");
 let readyToPurchase = false;
 let itemNameArray = [];
 let itemNumberArray = [];
 let itemCostArray = [];
+let totalItemCost = 0;
 
 
 // My Database Connection///////////////////
@@ -37,64 +50,64 @@ const initializeRunTheShop = () => {
             type: "input",
             name: "userName",
             message: "What's your name pirate?"
-        },
-        {
-            type: "list",
-            name: "itemName",
-            message: "Welcome in! What can I get for you today?",
-            choices: ["Coors Light", "Kraft Cheese", "48oz of Salmon", "TV", "Hydro Flask",
-                "PS4", "Kidney", "Backpack", "G.I. Joe", "6oz of Liquid Courage"]
-        },
-        {
-            type: "input",
-            name: "itemAmount",
-            message: "How many do you want?",
-            validate: function (value) {
-                if (isNaN(value) == false) {
-                    return true;
-                } else {
-                    console.log("\n" + "Woah there! Real pirates only ask in numbers");
-                    return false;
-                }
-            }
         }
-
 
     ]).then(function (user) {
         if (user.welcome === !true) {
             console.log("  Well if you're not going to use this bootleg service, try our other free products, such as 'Piratebay'," + "\n" +
                 " 'Fakebook', and 'Pirategram'. Have a great bootlegging day ya beautiful pirate!");
             process.exit();
+            connection.end();
         } else {
-            itemFinder(user.itemName, user.itemAmount, user.userName);
+            shoppingList(user.itemName, user.itemAmount, user.userName, totalItemCost);
         }
 
     });
 
 }
 
+////This function finds and displays information about each product. It is also responsible for the logic behind not letting the user purchase too much of something 
 const itemFinder = (itemName, itemAmount, userName, itemCost) => {
     console.log("Finding and selecting your product...\n");
-    connection.query("SELECT * FROM products WHERE product_name = " + "'" + itemName + "'", function (err, res) {
+    var query = connection.query("SELECT * FROM products WHERE product_name = " + "'" + itemName + "'", function (err, res) {
         if (err) throw err;
+
         let itemCost = res[0].price;
-        let totalItemCost = itemCost * itemAmount;
+        totalItemCost = itemCost * itemAmount;
         let numberOfStoredItems = res[0].stock_quantity;
-        // dataMover(numberOfStoredItems);
+
         console.log("There are " + numberOfStoredItems + " of those " + itemName + " items." + "\n" + "At a cost of $" + itemCost + " each. " + "\n" + userName + ", you have just purchased " + itemAmount + " of them for a total of $"
-            + totalItemCost);
+            + totalItemCost + "\n");
 
         if (itemAmount > numberOfStoredItems) {
             console.log("Aye, oi see you're a greedy pirate " + userName + ". We don't have enough of that item! Try a smaller amount-" + "\n");
             itemAmountSelector(itemName, userName);
 
         } else {
+            updateDataBase(itemName, itemAmount, numberOfStoredItems);
             keepShoppingFunction(itemName, itemAmount, userName, totalItemCost);
         }
 
     });
-
 }
+
+// This function updates the database everytime the user picks an item.
+const updateDataBase = (itemName, itemAmount, numberOfStoredItems) => {
+    let updatedItemAmount = numberOfStoredItems - itemAmount;
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?", 
+        [
+          {
+            stock_quantity: updatedItemAmount
+          },
+          {
+            product_name: itemName
+          }
+        ]
+      );
+}
+
+///////This function displays the prompt for asking the user how many of something they want
 const itemAmountSelector = (itemName, userName, totalItemCost) => {
     inquirer.prompt([
         {
@@ -117,6 +130,7 @@ const itemAmountSelector = (itemName, userName, totalItemCost) => {
     });
 }
 
+//////This function asks the user if they want to keep shopping
 const keepShoppingFunction = (itemName, itemAmount, userName, totalItemCost) => {
     inquirer.prompt([
         {
@@ -129,7 +143,6 @@ const keepShoppingFunction = (itemName, itemAmount, userName, totalItemCost) => 
         itemNameArray.push(itemName);
         itemNumberArray.push(itemAmount)
         itemCostArray.push(totalItemCost);
-        // updateDataBase(itemName, itemAmount, userName, totalItemCost);
 
         if (user.keepShopping == true) {
             shoppingList(itemName, itemAmount, userName, totalItemCost);
@@ -142,6 +155,8 @@ const keepShoppingFunction = (itemName, itemAmount, userName, totalItemCost) => 
 
 }
 
+
+/////This function displays my shopping list
 const shoppingList = (itemName, itemAmount, userName, totalItemCost) => {
     inquirer.prompt([
         {
@@ -159,6 +174,7 @@ const shoppingList = (itemName, itemAmount, userName, totalItemCost) => {
 
 }
 
+//////This function handles cashing the user out and ending the application
 const cartExecution = (itemName, itemAmount, userName, totalItemCost) => {
     inquirer.prompt([
         {
@@ -168,40 +184,19 @@ const cartExecution = (itemName, itemAmount, userName, totalItemCost) => {
         }
 
     ]).then(function (user) {
-        if(user.finishTheCart == false){
+        if (user.finishTheCart == false) {
             shoppingList(itemName, itemAmount, userName, totalItemCost);
             console.log("Well then, let's keep shopping");
 
-        }else{
+        } else {
             let finalCostAmount = itemCostArray.reduce((total, amount) => total + amount);
-            console.log("We went a head and processed your order for a total of $" + finalCostAmount + ". Thank you for shopping with us!" );
+            console.log("We went a head and processed your order for a total of $" + finalCostAmount + ". Thank you for shopping with us!");
             connection.end();
         }
     });
 }
 
-// const updateDataBase = (itemName, itemAmount) =>{
 
-//     connection.query("SELECT * FROM products WHERE product_name = " + "'" + itemName + "'", function (err, res) {
-//         let numberOfStoredItems = res[0].stock_quantity;
-
-//         let updatedItemAmount = numberOfStoredItems - itemAmount;
-//         var query = connection.query(
-//             "UPDATE products SET ? WHERE ?",
-//             [
-//               {
-//                 quantity: updatedItemAmount
-//               },
-//               {
-//                 department_name: itemName
-//               }
-//             ]
-//           );
-//     });
-
-
-
-// }
 
 
 
